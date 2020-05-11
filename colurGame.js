3ad0f8976c51af1306a14c46c8c67c1d4a1adc5d
Abbas Mokhtari <49838027+abbasmokhtari@users.noreply.github.com>
2020-05-11 21:17:15 +0100

    made sure we hard coded the test

--- colurGame.js
+++ colurGame.js
@@ -11,5 +11,26 @@ let colours = [
 let squares = document.querySelectorAll('.squares');
 
 for (let i = 0; i < squares.length; i++) {
+	//assign colour to each square
 	squares[i].style.backgroundColor = colours[i];
+
+	//write an event which picks the colour of the selected box
+	squares[i].addEventListener('click', function() {
+        // grab colour of the picked square
+        let clickedColour = this.style.backgroundColor;
+        //compare colour to picked colour
+        if (clickedColour === pickedColour) {
+            alert('correct');
+        } else {
+            alert('wrong');
+        }
+	});
 }
+
+//we want to pick a color as goal
+
+let pickedColour = colours[3];
+
+let shownColour = document.querySelector('h2');
+
+shownColour.textContent = pickedColour.toUpperCase();
