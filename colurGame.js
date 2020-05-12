3e327ad36fb88800c1ad764de224901dae837952
Abbas Mokhtari <49838027+abbasmokhtari@users.noreply.github.com>
2020-05-12 08:42:26 +0100

    made some css Grid styling

--- colurGame.js
+++ colurGame.js
@@ -3,7 +3,7 @@ let colours = generateRandomColours(6)
 
 let squares = document.querySelectorAll('.squares');
 let message = document.querySelector('#message');
-let h1 = document.querySelector('h1');
+let header = document.querySelector('header');
 
 for (let i = 0; i < squares.length; i++) {
 	//assign colour to each square
@@ -17,7 +17,7 @@ for (let i = 0; i < squares.length; i++) {
         if (clickedColour === pickedColour) {
             message.textContent = 'Correct';
             changeColour(clickedColour);
-            h1.style.backgroundColor = clickedColour;
+            header.style.backgroundColor = clickedColour;
         } else {
             this.style.backgroundColor = '#232323';
             message.textContent = 'Try agian';
